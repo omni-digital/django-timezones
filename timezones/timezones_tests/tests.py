e7012b20e047b9e78a53fc7814cd15cbbc8ab118
allthatilk <imogen.kutz@omnifarious-group.com>
2018-09-10 15:15:15 +0100

    Edit tests location and name and run through 2to3

--- timezones/timezones_tests/tests.py
+++ timezones/timezones_tests/tests.py
@@ -16,26 +16,26 @@ from timezones.utils import localtime_for_timezone, adjust_datetime_to_timezone
 
 
 class TimeZoneTestCase(TestCase):
-    
+
     def setUp(self):
         # ensure UTC
         self.ORIGINAL_TIME_ZONE = settings.TIME_ZONE
         settings.TIME_ZONE = "UTC"
-    
+
     def tearDown(self):
         settings.TIME_ZONE = self.ORIGINAL_TIME_ZONE
-    
+
     # little helpers
-    
+
     def assertFormIsValid(self, form):
         is_valid = form.is_valid()
-        self.assert_(is_valid,
+        self.assertTrue(is_valid,
             "Form did not validate (errors=%r, form=%r)" % (form._errors, form)
         )
 
 
 class UtilsTestCase(TimeZoneTestCase):
-    
+
     def test_localtime_for_timezone(self):
         self.assertEqual(
             localtime_for_timezone(
@@ -43,7 +43,7 @@ class UtilsTestCase(TimeZoneTestCase):
             ).strftime("%m/%d/%Y %H:%M:%S"),
             "06/25/2008 12:00:00"
         )
-    
+
     def test_adjust_datetime_to_timezone(self):
         self.assertEqual(
             adjust_datetime_to_timezone(
@@ -54,7 +54,7 @@ class UtilsTestCase(TimeZoneTestCase):
 
 
 class TimeZoneFieldTestCase(TimeZoneTestCase):
-    
+
     def test_forms_clean_required(self):
         f = timezones.forms.TimeZoneField()
         self.assertEqual(
@@ -62,7 +62,7 @@ class TimeZoneFieldTestCase(TimeZoneTestCase):
             "<DstTzInfo 'US/Eastern' EST-1 day, 19:00:00 STD>"
         )
         self.assertRaises(forms.ValidationError, f.clean, "")
-    
+
     def test_forms_clean_not_required(self):
         f = timezones.forms.TimeZoneField(required=False)
         self.assertEqual(
@@ -70,32 +70,32 @@ class TimeZoneFieldTestCase(TimeZoneTestCase):
             "<DstTzInfo 'US/Eastern' EST-1 day, 19:00:00 STD>"
         )
         self.assertEqual(f.clean(""), "")
-    
+
     def test_forms_clean_bad_value(self):
         f = timezones.forms.TimeZoneField()
         try:
             f.clean("BAD VALUE")
-        except forms.ValidationError, e:
+        except forms.ValidationError as e:
             self.assertEqual(e.messages, ["Select a valid choice. BAD VALUE is not one of the available choices."])
-    
+
     def test_models_as_a_form(self):
         class ProfileForm(forms.ModelForm):
             class Meta:
                 model = test_models.Profile
         form = ProfileForm()
         rendered = form.as_p()
-        self.assert_(
+        self.assertTrue(
             bool(re.search(r'<option value="[\w/]+">\([A-Z]+(?:\+|\-)\d{4}\)\s[\w/]+</option>', rendered)),
             "Did not find pattern in rendered form"
         )
-    
+
     def test_models_modelform_validation(self):
         class ProfileForm(forms.ModelForm):
             class Meta:
                 model = test_models.Profile
         form = ProfileForm({"name": "Brian Rosner", "timezone": "America/Denver"})
         self.assertFormIsValid(form)
-    
+
     def test_models_modelform_save(self):
         class ProfileForm(forms.ModelForm):
             class Meta:
@@ -103,25 +103,25 @@ class TimeZoneFieldTestCase(TimeZoneTestCase):
         form = ProfileForm({"name": "Brian Rosner", "timezone": "America/Denver"})
         self.assertFormIsValid(form)
         p = form.save()
-    
+
     def test_models_string_value(self):
         p = test_models.Profile(name="Brian Rosner", timezone="America/Denver")
         p.save()
         p = test_models.Profile.objects.get(pk=p.pk)
         self.assertEqual(p.timezone, pytz.timezone("America/Denver"))
-    
+
     def test_models_string_value_lookup(self):
         test_models.Profile(name="Brian Rosner", timezone="America/Denver").save()
         qs = test_models.Profile.objects.filter(timezone="America/Denver")
         self.assertEqual(qs.count(), 1)
-    
+
     def test_models_tz_value(self):
         tz = pytz.timezone("America/Denver")
         p = test_models.Profile(name="Brian Rosner", timezone=tz)
         p.save()
         p = test_models.Profile.objects.get(pk=p.pk)
         self.assertEqual(p.timezone, tz)
-    
+
     def test_models_tz_value_lookup(self):
         test_models.Profile(name="Brian Rosner", timezone="America/Denver").save()
         qs = test_models.Profile.objects.filter(timezone=pytz.timezone("America/Denver"))
@@ -129,7 +129,7 @@ class TimeZoneFieldTestCase(TimeZoneTestCase):
 
 
 class LocalizedDateTimeFieldTestCase(TimeZoneTestCase):
-    
+
     def test_forms_clean_required(self):
         # the default case where no timezone is given explicitly. uses settings.TIME_ZONE.
         f = timezones.forms.LocalizedDateTimeField()
@@ -138,7 +138,7 @@ class LocalizedDateTimeFieldTestCase(TimeZoneTestCase):
             "datetime.datetime(2008, 5, 30, 14, 30, tzinfo=<UTC>)"
         )
         self.assertRaises(forms.ValidationError, f.clean, "")
-    
+
     def test_forms_clean_required(self):
         # the default case where no timezone is given explicitly. uses settings.TIME_ZONE.
         f = timezones.forms.LocalizedDateTimeField(required=False)
